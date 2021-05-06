967dd6f195ea43ba6311a6ad0ef912cdbf0b0068
Jura Slapnicka <jiri.slapnicka@gmail.com>
2021-05-06 20:40:49 +0200

    validate function added

--- trainer/srcnn/trainer.py
+++ trainer/srcnn/trainer.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import matplotlib
 import matplotlib.pyplot as plt
 import h5py
+from torchvision.utils import save_image
 from tqdm import tqdm
 
 from torch.utils.data import DataLoader
@@ -20,11 +21,11 @@ epochs = 100
 lr = 0.001
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# input image dimensions
+# inputs image dimensions
 img_rows, img_cols = 33, 33
 out_rows, out_cols = 33, 33
 
-file = h5py.File('../../data/input/tutorial/train_mscale.h5', mode='r')
+file = h5py.File('../../data/inputs/tutorial/train_mscale.h5', mode='r')
 # in_train has shape (21824, 33, 33, 1) => 21824 images of size 33x33 with 1 color channel
 in_train = file['data'][:] # training data
 out_train = file['label'][:] # training labels
@@ -53,6 +54,7 @@ optimizer = optim.Adam(model.parameters(), lr=lr)
 # loss function
 criterion = nn.MSELoss()
 
+
 def train(model, dataloader):
     model.train()
     running_loss = 0.0
@@ -79,4 +81,29 @@ def train(model, dataloader):
 
     final_loss = running_loss / len(dataloader.dataset)
     final_psnr = running_psnr / int(len(train_data) / dataloader.batch_size)
+    return final_loss, final_psnr
+
+
+def validate(model, dataloader, epoch):
+    model.eval()
+    running_loss = 0.0
+    running_psnr = 0.0
+    with torch.no_grad():
+        for bi, data in tqdm(enumerate(dataloader), total=int(len(val_data) / dataloader.batch_size)):
+            image_data = data[0].to(device)
+            label = data[1].to(device)
+
+            outputs = model(image_data)
+            loss = criterion(outputs, label)
+
+            # add loss to each item (total items in a batch = batch size)
+            running_loss += loss.item()
+            # calculate batch psnr (once every 'batch_size' iterations)
+            batch_psnr = psnr(label, outputs)
+            running_psnr += batch_psnr
+        outputs = outputs.cpu()
+        save_image(outputs, f'../../data/outputs/val_sr{epoch}.png')
+
+    final_loss = running_loss / len(dataloader.dataset)
+    final_psnr = running_psnr / int(len(val_data) / dataloader.batch_size)
     return final_loss, final_psnr
